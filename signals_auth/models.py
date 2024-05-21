2122fe64755dc00b194528681f02e5607691261d
unknown <michaelibeh12@gmail.com>
2024-05-21 09:13:51 +0100

    removed connnected clients model

--- signals_auth/models.py
+++ signals_auth/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
