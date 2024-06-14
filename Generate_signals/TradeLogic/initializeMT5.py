0bbeef610d1303d7ae722ff3231e995b09d810b3
unknown <michaelibeh12@gmail.com>
2024-06-14 17:27:12 +0100

    update

--- Generate_signals/TradeLogic/initializeMT5.py
+++ Generate_signals/TradeLogic/initializeMT5.py
@@ -18,8 +18,8 @@ def start_mt5():
     else:
         # print("MT5 initialized successfully")
         logger.info(f"MT5 initialized successfully")
-        account = 82288340
-        password = 'H@W6ZaSa'
+        account = 83037510
+        password = 'LlU+D4Er'
         server = 'MetaQuotes-Demo'
         login = mt5.login(login=account, password=password, server=server)
 
